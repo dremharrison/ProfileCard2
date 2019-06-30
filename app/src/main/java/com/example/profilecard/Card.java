f67b6de6d650071935dffde40b593af8eb6fbcf5
Dre <deandremharrison@gmail.com>
2019-06-29 20:30:32 -0400

    dropdown and contract button function

--- app/src/main/java/com/example/profilecard/Card.java
+++ app/src/main/java/com/example/profilecard/Card.java
@@ -2,8 +2,10 @@ package com.example.profilecard;
 
 
 import android.os.Bundle;
-import android.widget.ImageView;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 import androidx.annotation.Nullable;
@@ -11,11 +13,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Card extends AppCompatActivity {
 
-
+ImageButton btnDropdown, btnContract;
+TextView moreInfo;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
+
+        btnDropdown = findViewById(R.id.expand_button);
+        btnContract = findViewById(R.id.contract_button);
+        moreInfo = findViewById(R.id.supporting_text);
+
+        btnDropdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnDropdown.setVisibility(View.GONE);
+                btnContract.setVisibility(View.VISIBLE);
+                moreInfo.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnContract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnContract.setVisibility(View.GONE);
+                moreInfo.setVisibility(View.GONE);
+                btnDropdown.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
